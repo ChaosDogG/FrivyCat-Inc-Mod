4e44ac895ecb61b2fb9195a76d1ecbeea5ff83b5
Noah <noahtjelen@gmail.com>
2021-08-02 10:16:29 -0700

    Started work on the Stone Banana

---
+++ src/main/java/com/chaosdog/frivycat/items/StoneBanana.java
@@ -0,0 +1,14 @@
+package com.chaosdog.frivycat.items;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.ToolItem;
+
+import java.util.Set;
+
+// this will be the class that will represent the stone banana
+public class StoneBanana extends ToolItem {
+    public StoneBanana(float p_i48512_1_, float p_i48512_2_, IItemTier tier, Set<Block> blocks, Properties p_i48512_5_) {
+        super(p_i48512_1_, p_i48512_2_, tier, blocks, p_i48512_5_);
+    }
+}
