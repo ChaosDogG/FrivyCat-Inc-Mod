c880f2b36a38e47a4605724407c2ce12c0d77fbb
ChaosDogG <69123583+ChaosDogG@users.noreply.github.com>
2021-08-09 07:30:53 -0700

    Found a grammar error

--- src/main/java/com/chaosdog/frivycat/items/ShinyItem.java
+++ src/main/java/com/chaosdog/frivycat/items/ShinyItem.java
@@ -3,7 +3,7 @@ package com.chaosdog.frivycat.items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
-// custom class for items that need to be shiny (in as having the enchanted glint effect)
+// custom class for items that need to be shiny (as in having the enchanted glint effect)
 // it is constructed the same way as regular items
 public class ShinyItem extends Item {
     public ShinyItem(Properties properties) {
