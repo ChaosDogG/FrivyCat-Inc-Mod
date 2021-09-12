8ff4f9459ed171e94dbb586affd0fdb6a0232474
ChaosDogG <attiecatgreen@gmail.com>
2021-09-11 20:45:45 -0700

    Was dumbass and forgot to make gem blocks uncraft to gems

---
+++ scripts/gen_gem_from_block_recipe.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+gems = [
+    "ruby",
+    "sapphire",
+    "zircon",
+    "garnet",
+    "jade",
+    "jasper",
+    "spinel",
+    "topaz",
+    "agate"
+]
+
+recipe = "{{\n    \"type\": \"minecraft:crafting_shapeless\",\n\"ingredients\": [\n    {{\n    \"item\": \"frivycat: {t}_block\"\n}}\n],\n\"result\": {{\n\"item\": \"frivycat:{t}\",\n\"count\": 9\n}}\n}}"
+
+for gem in gems:
+    name = gem + ".json"
+    content = recipe.format(t = gem);
+    file = open(name, "w")
+    file.write(content)
+    file.close()
+
+print("Done")
--- src/main/resources/data/frivycat/functions/madness_diamond_fix.mcfunction
+++
@@ -1,2 +0,0 @@
-clear @s diamond{CustomModelData:11}
-give @s diamond{CustomModelData:11,Enchantments:[{id:69}],display:{Name:'[{"text":"Madness ","color":"green","italic":false},{"text":"Diamond","color":"dark_purple","italic":false}]'}}
