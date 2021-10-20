80da1dbfd729cbac69fbf4a4edd5ff473f41df65
Noah <noahtjelen@gmail.com>
2021-10-20 13:08:48 -0700

    Disabled the experimental world warning screen

---
+++ src/main/java/chaosdog/frivycat/mixin/ServerWorldInfoMixin.java
@@ -0,0 +1,18 @@
+package chaosdog.frivycat.mixin;
+
+import chaosdog.frivycat.FrivyCatMod;
+import com.mojang.serialization.Lifecycle;
+import net.minecraft.world.storage.ServerWorldInfo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ServerWorldInfo.class)
+public abstract class ServerWorldInfoMixin {
+    @Inject(method = "getLifecycle", at = @At("HEAD"), cancellable = true)
+    private void noExperimentalWarning(CallbackInfoReturnable<Lifecycle> lifecycle) {
+        lifecycle.setReturnValue(Lifecycle.stable());
+        FrivyCatMod.LOG.debug("NetherNoah777: Dear Mojang, if we are using mods, we are already aware that experimental world settings are unsupported because mods are unsupported by you guys as well!");
+    }
+}
