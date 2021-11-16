f67f90bd36b610304ee7da0521cc40968781915d
ChaosDogG <attiecatgreen@gmail.com>
2021-11-16 15:29:38 -0700

    Red Tea does something!

---
+++ src/main/java/chaosdog/frivycat/items/RedTeaBucket.java
@@ -0,0 +1,34 @@
+package chaosdog.frivycat.items;
+
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.MilkBucketItem;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.World;
+
+public class RedTeaBucket extends MilkBucketItem {
+    public RedTeaBucket(Properties builder) {
+        super(builder);
+    }
+
+    @Override
+    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
+        if (!worldIn.isRemote) entityLiving.curePotionEffects(stack); // FORGE - move up so stack.shrink does not turn stack into air
+
+        if (entityLiving instanceof ServerPlayerEntity) {
+            ServerPlayerEntity serverplayerentity = (ServerPlayerEntity)entityLiving;
+            CriteriaTriggers.CONSUME_ITEM.trigger(serverplayerentity, stack);
+            serverplayerentity.addStat(Stats.ITEM_USED.get(this));
+        }
+
+        if (entityLiving instanceof PlayerEntity && !((PlayerEntity)entityLiving).abilities.isCreativeMode) {
+            stack.shrink(1);
+        }
+
+        return stack.isEmpty() ? new ItemStack(Items.BUCKET) : stack;
+    }
+}
