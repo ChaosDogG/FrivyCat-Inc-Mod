bf08c82b5c1eba498d2058d5dcc42be5d09f3b1d
Noah <noahtjelen@gmail.com>
2021-11-19 14:22:11 -0700

    Special diamonds improvements

--- src/main/java/chaosdog/frivycat/SpecialDiamonds.java
+++ src/main/java/chaosdog/frivycat/SpecialDiamonds.java
@@ -1,6 +1,7 @@
 package chaosdog.frivycat;
 
 import chaosdog.frivycat.items.ShinyItem;
+import chaosdog.frivycat.items.SpecialDiamond;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Rarity;
@@ -14,21 +15,20 @@ public class SpecialDiamonds {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FrivyCatMod.ID);
 
     // special diamonds
-    public static final RegistryObject<Item> DARK_DIAMOND = regSpecialDiamond("dark");
-    public static final RegistryObject<Item> SHADOW_DIAMOND = regSpecialDiamond("shadow");
-    public static final RegistryObject<Item> CULTIST_DIAMOND = regSpecialDiamond("cultist");
-    public static final RegistryObject<Item> FIRE_DIAMOND = regSpecialDiamond("fire");
-    public static final RegistryObject<Item> LIGHTNING_DIAMOND = regSpecialDiamond("lightning");
-    public static final RegistryObject<Item> PEACE_DIAMOND = regSpecialDiamond("peace");
-    public static final RegistryObject<Item> VINE_DIAMOND = regSpecialDiamond("vine");
-    public static final RegistryObject<Item> VETERAN_DIAMOND = regSpecialDiamond("veteran");
-    public static final RegistryObject<Item> HOLY_DIAMOND = regSpecialDiamond("holy");
-    public static final RegistryObject<Item> WATER_DIAMOND = regSpecialDiamond("water");
-    public static final RegistryObject<Item> MADNESS_DIAMOND = regSpecialDiamond("madness");
-    public static final RegistryObject<Item> CORRUPTED_DIAMOND = regSpecialDiamond("corrupted");
-    // should not be shiny
-    public static final RegistryObject<Item> CATALYST_DIAMOND = regSpecialDiamond("catalyst");
-    public static final RegistryObject<Item> DRAINED_DIAMOND = regSpecialDiamond("drained");
+    public static final RegistryObject<Item> DARK_DIAMOND = regSpecialDiamond("dark", true);
+    public static final RegistryObject<Item> SHADOW_DIAMOND = regSpecialDiamond("shadow", true);
+    public static final RegistryObject<Item> CULTIST_DIAMOND = regSpecialDiamond("cultist", true);
+    public static final RegistryObject<Item> FIRE_DIAMOND = regSpecialDiamond("fire", true);
+    public static final RegistryObject<Item> LIGHTNING_DIAMOND = regSpecialDiamond("lightning", true);
+    public static final RegistryObject<Item> PEACE_DIAMOND = regSpecialDiamond("peace", true);
+    public static final RegistryObject<Item> VINE_DIAMOND = regSpecialDiamond("vine", true);
+    public static final RegistryObject<Item> VETERAN_DIAMOND = regSpecialDiamond("veteran", true);
+    public static final RegistryObject<Item> HOLY_DIAMOND = regSpecialDiamond("holy", true);
+    public static final RegistryObject<Item> WATER_DIAMOND = regSpecialDiamond("water", true);
+    public static final RegistryObject<Item> MADNESS_DIAMOND = regSpecialDiamond("madness", true);
+    public static final RegistryObject<Item> CORRUPTED_DIAMOND = regSpecialDiamond("corrupted", true);
+    public static final RegistryObject<Item> CATALYST_DIAMOND = regSpecialDiamond("catalyst", false);
+    public static final RegistryObject<Item> DRAINED_DIAMOND = regSpecialDiamond("drained", false);
 
     public static void init(IEventBus eventbus) {
         FrivyCatMod.LOG.info("Setting up Special Diamonds");
@@ -37,7 +37,7 @@ public class SpecialDiamonds {
     }
 
     // registers a special diamond item
-    private static RegistryObject<Item> regSpecialDiamond(String name) {
-        return Utils.regItem(ITEMS, name + "_diamond", new ShinyItem(new Item.Properties().group(ItemGroup.MISC).rarity(Rarity.EPIC)));
+    private static RegistryObject<Item> regSpecialDiamond(String name, boolean isShiny) {
+        return Utils.regItem(ITEMS, name + "_diamond", new SpecialDiamond(isShiny));
     }
 }
