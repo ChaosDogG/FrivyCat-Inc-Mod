7220e4058e0508bd6638e39b73dc88f23320024f
ChaosDogG <attiecatgreen@gmail.com>
2021-12-04 16:11:42 -0700

    JEI support pog!

--- src/main/java/chaosdog/frivycat/integrations/jei/FrivyCatJei.java
+++ src/main/java/chaosdog/frivycat/integrations/jei/FrivyCatJei.java
@@ -1,4 +1,38 @@
 package chaosdog.frivycat.integrations.jei;
 
-public class FrivyCatJei {
+import chaosdog.frivycat.FrivyCatMod;
+import chaosdog.frivycat.Gems;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+@JeiPlugin
+public class FrivyCatJei implements IModPlugin {
+
+    private static final ResourceLocation UID = new ResourceLocation(FrivyCatMod.ID, "jei");
+
+    private void addDescription(IRecipeRegistration registration, ItemStack item) {
+        registration.addIngredientInfo(item, VanillaTypes.ITEM, item.getTranslationKey() + ".jei_info");
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        addDescription(registration, new ItemStack(Gems.RUBY.get()));
+        addDescription(registration, new ItemStack(Gems.SAPPHIRE.get()));
+        addDescription(registration, new ItemStack(Gems.ZIRCON.get()));
+        addDescription(registration, new ItemStack(Gems.GARNET.get()));
+        addDescription(registration, new ItemStack(Gems.JADE.get()));
+        addDescription(registration, new ItemStack(Gems.JASPER.get()));
+        addDescription(registration, new ItemStack(Gems.SPINEL.get()));
+        addDescription(registration, new ItemStack(Gems.TOPAZ.get()));
+        addDescription(registration, new ItemStack(Gems.AGATE.get()));
+    }
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return UID;
+    }
 }
