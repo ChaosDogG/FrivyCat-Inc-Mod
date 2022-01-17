20e906debae8ae2fdb01244587c6d065e78e414e
Noah <noahtjelen@gmail.com>
2022-01-16 22:55:26 -0700

    The mobs now spawn correctly in their correct biomes!

--- src/main/java/chaosdog/frivycat/world/FCWorldEvents.java
+++ src/main/java/chaosdog/frivycat/world/FCWorldEvents.java
@@ -1,13 +1,13 @@
 package chaosdog.frivycat.world;
 
 import chaosdog.frivycat.FrivyCatMod;
-import chaosdog.frivycat.world.gen.ModEntityGeneration;
+import chaosdog.frivycat.entities.ModEntityTypes;
 import chaosdog.frivycat.world.gen.structures.FCStructures;
-import net.minecraft.util.RegistryKey;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.biome.Biome;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.world.biome.MobSpawnInfo;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraftforge.common.world.MobSpawnInfoBuilder;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -19,13 +19,28 @@ import java.util.function.Supplier;
 public class FCWorldEvents {
 
     @SubscribeEvent
-    public static void biomeLoadingEvent(final BiomeLoadingEvent event) {
-        RegistryKey<Biome> key = RegistryKey.getOrCreateKey(Registry.BIOME_KEY, event.getName());
-
+    public static void initWorldGen(final BiomeLoadingEvent event) {
+        // structure generation
         List<Supplier<ConfiguredFeature<?, ?>>> structureGen = event.getGeneration().getFeatures(GenerationStage.Decoration.SURFACE_STRUCTURES);
         structureGen.add(() -> FCStructures.CHAOS_HEAD);
         structureGen.add(() -> FCStructures.VILLAGER_SPAWNER);
         structureGen.add(() -> FCStructures.DFAW);
-        ModEntityGeneration.onEntitySpawn(event);
+
+        // mob spawns
+        MobSpawnInfoBuilder spawnInfo = event.getSpawns();
+
+        switch(event.getName().toString()) {
+            case "minecraft:plains":
+                spawnInfo.withSpawner(EntityClassification.AMBIENT, new MobSpawnInfo.Spawners(ModEntityTypes.PIGPER.get(), 20, 1, 4));
+                break;
+
+            case "minecraft:dark_forest": case "minecraft:dark_forest_hills":
+                spawnInfo.withSpawner(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(ModEntityTypes.SCOOBY_SKELETON.get(), 20, 1, 4));
+                break;
+
+            case "minecraft:snowy_tundra":
+                spawnInfo.withSpawner(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(ModEntityTypes.SCOOBY_STRAY.get(), 20, 1, 4));
+                break;
+        }
     }
 }
